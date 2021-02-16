39aae2092291b627ff4c85265d4b395baa9f6bc8
Stephen-prog <stev.monye04@gmail.com>
2021-02-16 00:02:36 -1100

    Globe bank

---
+++ private/shared/staff_header.php
@@ -0,0 +1,22 @@
+<?php
+    if (!isset($page_title)) { $page_title = 'Staff Area'; }
+?>
+<!DOCTYPE html>
+
+<html lang="en"> 
+<head>
+    <title>GBI - <?php echo h($page_title); ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>">
+</head>
+<body>
+    <header>
+        <h1>GBI staff Area</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
+        </ul>
+    </nav>
