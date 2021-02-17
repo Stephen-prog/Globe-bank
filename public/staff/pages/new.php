1e7a8a929ed6decc1f4a35155652695625122add
Stephen-prog <stev.monye04@gmail.com>
2021-02-17 00:25:24 -1100

    modified

--- public/staff/pages/new.php
+++ public/staff/pages/new.php
@@ -22,8 +22,6 @@ if (is_post_request()) {
 
 ?>
 
-
-
 <?php $page_title = 'Create Subject'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
@@ -37,13 +35,13 @@ if (is_post_request()) {
         <form action="<?php echo url_for('/staff/pages/new.php'); ?>" method="post">
             <dl>
                 <dt>Menu Name</dt>
-                <dd><input type="text" name="menu_name" value=""></dd>
+                <dd><input type="text" name="menu_name" value="<?php echo h($menu_name); ?>"></dd>
             </dl>
             <dl>
                 <dt>Position</dt>
                 <dd>
                     <select name="position">
-                        <option value="1">1</option>
+                        <option value="<?php if($position == "1") { echo " selected"; } ?>">1</option>
                     </select>
                 </dd>
             </dl>
@@ -51,7 +49,7 @@ if (is_post_request()) {
                 <dt>Visible</dt>
                 <dd>
                     <input type="hidden" name="visible" value="0">
-                    <input type="checkbox" name="visible" value="1">
+                    <input type="checkbox" name="visible" value="1"<?php if($visible == "1") {echo " checked"; } ?> >
                 </dd>
             </dl>
             <div id="operations">
